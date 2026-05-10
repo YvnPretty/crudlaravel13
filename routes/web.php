4c2927bddccd6e01342f0c12591922c9a48dd2c6
YvnPretty <YvnPretty@users.noreply.github.com>
2026-05-09 23:54:38 -0600

    Redirect root to productos index

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('productos.index');
 });
 
 Route::resource('productos', ProductoController::class);
